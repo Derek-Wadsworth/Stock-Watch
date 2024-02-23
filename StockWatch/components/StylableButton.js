5856cba7b96e612a5813d2424956089823177504
Derek Wadsworth <derektwadsworth@gmail.com>
2024-02-22 21:20:16 -0800

    added disabled prop
    This allows for buttons to be disabled until a desired condition is me (for example, a password has length > 5)

--- StockWatch/components/StylableButton.js
+++ StockWatch/components/StylableButton.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
 
-const StylableButton = ({ text, onPress, style }) => {
+const StylableButton = ({ text, onPress, disabled, style }) => {
     return(
-        <TouchableOpacity onPress={onPress} style={style[0]}>
+        <TouchableOpacity onPress={onPress} style={style[0]} disabled={disabled}>
             <View style={style[1]}>
                 <Text style={style[2]}>{text}</Text>
             </View>
